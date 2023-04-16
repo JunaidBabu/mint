66a9310cf4bad487994c968b34d10c92a827a7fa
Junaid Babu <junaidvayoli@gmail.com>
2023-04-16 05:26:46 +0000

    grep ip

--- script.py
+++ script.py
@@ -4,4 +4,8 @@ import requests
 token = os.environ['JUNBOT_TOKEN']
 chat_id = os.environ['CHAT_ID']
 
-requests.get("https://api.telegram.org/bot"+token+"/sendmessage?chat_id="+chat_id+"&text=SellNow")
+# get global ip address
+ip = requests.get("https://api.ipify.org").text
+# print(ip)
+
+requests.get("https://api.telegram.org/bot"+token+"/sendmessage?chat_id="+chat_id+"&text="+ip)
